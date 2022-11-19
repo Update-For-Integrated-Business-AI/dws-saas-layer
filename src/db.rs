6613e913dcae897348b9d2bd71eb636db99840f5
Bahy <35463814+bahyali@users.noreply.github.com>
2022-11-19 20:44:38 +0200

    feat: get table rows

--- src/db.rs
+++ src/db.rs
@@ -12,7 +12,22 @@ fn get_column_names(content: &String) -> Vec<String> {
             .map(|f| String::from(f.trim()))
             .collect::<Vec<String>>(),
         None => panic!("Table has no columns"),
+    };
+}
+
+fn get_rows(content: &String) -> Vec<Vec<String>> {
+    let mut rows = vec![];
+    for line in content.lines().skip(1) {
+        let row = match Some(line) {
+            Some(l) => l
+                .split(',')
+                .map(|f| String::from(f.trim()))
+                .collect::<Vec<String>>(),
+            None => panic!("Table has no rows"),
+        };
+        rows.push(row);
     }
+    rows
 }
 
 pub fn run() -> String {
@@ -30,17 +45,32 @@ pub trait SearchableList {
 #[cfg(test)]
 mod tests {
     use super::*;
-    
+
     #[test]
     fn test_extracting_column_names_from_file() {
-
         let table = "column1, column2, column3
         row1_value1, row1_value2, row1_value3
         row2_value1, row2_value2, row2_value3";
 
         let columns = get_column_names(&String::from(table));
 
-
         assert_eq!(columns, vec!["column1", "column2", "column3"])
+    }
+
+    #[test]
+    fn test_extracting_rows_from_file() {
+        let table = "column1, column2, column3
+        row1_value1, row1_value2, row1_value3
+        row2_value1, row2_value2, row2_value3";
+
+        let columns = get_rows(&String::from(table));
+
+        assert_eq!(
+            columns,
+            vec![
+                vec!["row1_value1", "row1_value2", "row1_value3"],
+                vec!["row2_value1", "row2_value2", "row2_value3"],
+            ]
+        )
     }
 }
